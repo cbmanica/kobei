585dd19b9be87978b2bf76141b1a6cab1cd13979
C. Benson Manica <cbmanica@gmail.com>
2013-05-12 12:47:36 -0700

    app skeleton

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kobei::Application.config.secret_token = '58baeff88d76afd8f618c78bb80c72554d93aa265b426817c5d131d1796cb855a41ee1bf89abbd0f6f2364a17a70cd0d77d381decce27b01eabd45741d2fbf8e'
